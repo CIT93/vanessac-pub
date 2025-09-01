5038e7bca2c0eb021b3235e60ba1a64678b26245
Vanessa Castro <0640164@my.scccd.edu>
2025-08-31 20:45:42 -0700

    1st Attempt -  Greetings

---
+++ w4/app.js
@@ -0,0 +1,5 @@
+addEventListener(submit = "clicked");   // Listen for "submit"
+const event.preventDefault();           // Stop browser from reloading
+const [Name] = [Name].value;            // New greeting string
+const input ${"Hello, [Name]!"};        // Output string
+const = getElementById ("Greetings!")reset();   //Reset input
