0b99386980533e65ac9ade0cff1bffd8bbb2fbc2
Vanessa Castro <0640164@my.scccd.edu>
2025-09-01 15:40:19 -0700

    Updated Code - Fixed

--- w4/app.js
+++ w4/app.js
@@ -1,5 +1,22 @@
-addEventListener(submit = "clicked");   // Listen for "submit"
-const event.preventDefault();           // Stop browser from reloading
-const [Name] = [Name].value;            // New greeting string
-const input ${"Hello, [Name]!"};        // Output string
-const = getElementById ("Greetings!")reset();   //Reset input
+document.addEventListener("DOMContentLoaded", function() {              // Link to index.html
+    const formElement = document.getElementById("greetForm");   
+    const nameInput = document.getElementById("nameInput");
+    const messageElement = document.getElementById("messageElement");
+
+    formElement.addEventListener("submit", function(event) {            // Listens for "submit"               
+        event.preventDefault();                                         // Event handler
+        const name = nameInput.value;                                   // Reads text input
+
+        if (name) {                                                     
+            messageElement.textContent = `Hi ${name}, Have a good day!`;   // New string
+        } else {
+            messageElement.textContent = "Please enter a name";
+
+            form.reset();                                               // Reset input
+        }
+    });
+}); 
+
+// Reviewed code along videos and VSC
+// Gemini helped find and explain why code isn't working
+// Re-researched which commands were for what
